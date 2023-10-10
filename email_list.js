97f390190eb35313896f1f68086e48dfdddd7f4f
devsujatha <145225805+devsujatha@users.noreply.github.com>
2023-10-10 16:02:23 +0300

    Add files via upload
    Added documentation and images

---
+++ email_list.js
@@ -0,0 +1,33 @@
+"use strict";
+var $ = function(id) {
+    return document.getElementById(id);
+};
+var joinList = function() {
+    var emailAddress1 = $("email_address1").value;
+    var emailAddress2 = $("email_address2").value;
+    var firstName = $("first_name").value;
+    var errorMessage = "";
+    if (emailAddress1 == "") {
+    	errorMessage = "First email address entry required";
+        $("email_address1").focus();
+    } else if (emailAddress2 == "") {
+    	errorMessage = "Second email address entry required";
+    	$("email_address2").focus();
+    } else if (emailAddress2 != emailAddress1) {
+    	errorMessage = "Email address entries must match";
+    	$("email_address2").focus();
+    } else if (firstName == "") {
+    	errorMessage = "First name entry required";
+    	$("first_name").focus();
+    }
+    
+    if (errorMessage == "") {
+        $("email_form").submit(); 
+    } else {
+    	alert(errorMessage);
+    }
+};
+window.onload = function() {
+    $("join_list").onclick = joinList;
+    $("email_address1").focus();
+};
